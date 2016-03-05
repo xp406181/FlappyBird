251cef8744f7cf2336a488b0ce4232e2b9beb67b
王哲 <584018312@qq.com>
2016-03-06 02:01:22 +0800

    完善FlappyBird

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+	Rigidbody m_rigid;
+	public float m_VX = 5.0f;
+	public float m_VY = 3.0f;
+	// Use this for initialization
+	void Start () {
+		m_rigid = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Fire1")){
+			this.m_rigid.velocity = new Vector3(m_VX,m_VY,0);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	Transform m_BridTrans;
+	public Transform m_FirstBg;
+	float m_DefaultDiffX = 3f;
+	float m_DefaultDiffY = -2.5f;
+	float m_DefaultDiffZ  = -9.5f;
+
+	float m_MaxY = 1.36f;
+	float m_MinY = -1.39f;
+	// Use this for initialization
+	void Start () {
+		m_BridTrans = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = new Vector3(m_BridTrans.position.x + m_DefaultDiffX,m_BridTrans.position.y + m_DefaultDiffY, m_BridTrans.position.z + m_DefaultDiffZ);
+		if(newPos.y > m_MaxY)
+		{
+			newPos.y = m_MaxY;
+		}
+
+		if(newPos.y < m_MinY)
+		{
+			newPos.y = m_MinY;
+		}
+
+		this.transform.position = newPos;
+	}
+}
